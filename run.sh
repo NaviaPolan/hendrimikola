a89968fb3bbf8dd0083b77cd136178f7bdda70ac
NaviaPolan <161333610+NaviaPolan@users.noreply.github.com>
2024-03-23 16:58:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-5912a679-f4d4-40c7-8f63-1b33ed326504/)
 
 
 echo "$curl_output" >> Update.tsx
